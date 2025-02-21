a83c00908ed66d3a5d96b11a1ca1bb635f29e057
karlalacuna <karlaacunainca@gmail.com>
2025-02-21 00:17:20 -0300

    [NEW]Responsive NavBar

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,20 +1,47 @@
-import { Container, Navbar, Nav } from 'react-bootstrap';
-import '../styles/navBarStyle.css'
+import { Container, Navbar, Nav, Row, Col } from 'react-bootstrap';
+import { useEffect } from 'react';
+import '../styles/navBarStyle.css';
 
 const NavBar = () => {
+  useEffect(() => {
+    const handleScroll = () => {
+      const navbar = document.querySelector('.navbar-banner') as HTMLElement;
+      if (navbar) {
+        navbar.classList.toggle('down', window.scrollY > 0);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <Navbar bg="light" expand="lg" fixed='top' className='navbar-banner'>
+    <Navbar expand="lg" fixed="top" className="navbar-banner pt-3 pb-3">
       <Container>
-        <Navbar.Brand href="#home">
-        <img alt="la casa del puerko" className="navbar-image" src='/public/sticker.png'/></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link href="#menu">Menú</Nav.Link>
-            <Nav.Link href="#nosotros">¿Quiénes somos?</Nav.Link>
-            <Nav.Link href="#whatsapp">(+51) 989-974-555</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+        <Row className="w-100">
+          <Col className='d-block'></Col>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav>
+                <div  id='navbarright'>
+                <Nav.Link href="/">Inicio</Nav.Link>
+                <Nav.Link href="#menu">Menú</Nav.Link>
+                </div>
+                <div id='navbarleft'>
+                <Nav.Link href="#nosotros">Nosotros</Nav.Link>
+                <Nav.Link href="#whatsapp">(+51) 989-974-555</Nav.Link>
+                </div>
+              </Nav>
+            </Navbar.Collapse>
+          <Col className="d-flex justify-content-center" xs={12}>
+            <Navbar.Brand href="#home">
+              <img alt="la casa del puerko" className="navbar-image" src="/public/sticker.png" />
+            </Navbar.Brand>
+          </Col>
+        </Row>
       </Container>
     </Navbar>
   );
